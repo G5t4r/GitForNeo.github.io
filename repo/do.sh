99defda60190a319027a2c8e50cecd19e28ab6f0
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-01-24 10:52:14 +0800

    Repo

--- repo/do.sh
+++ repo/do.sh
@@ -1,15 +1,16 @@
+#!/bin/sh
 
-To make Packages on iPhone do:
+#To make Packages on iPhone do:
 ./dpkg-scanpackages debs /dev/null > Packages-new
 
-To make .gz and .bz2:
+#To make .gz and .bz2:
 rm Packages-new
 rm Packages.gz
 rm Packages.bz2
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 
-To push all to host:
+#To push all to host:
 git add -A
 git commit -m "Repo"
 git push -u origin master
