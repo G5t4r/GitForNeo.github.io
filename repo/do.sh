9a8cc980869376446dd9a4c3189649a45f1c2f80
Carl Vuorinen <carl.vuorinen@gmail.com>
2018-01-25 11:48:10 +0800

    Repo

--- repo/do.sh
+++ repo/do.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 #To make Packages on iPhone do:
-rm Packages
-./dpkg-scanpackages Files /dev/null > Packages
+# rm -f Packages
+# ./dpkg-scanpackages Files /dev/null > Packages
 
 #To make .gz and .bz2:
 # rm Packages
-rm Packages.gz
-rm Packages.bz2
+rm -f Packages.gz
+rm -f Packages.bz2
 gzip -c9 Packages > Packages.gz
 bzip2 -c9 Packages > Packages.bz2
 
